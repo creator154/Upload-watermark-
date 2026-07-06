9cd032cdc275a815fecedadb9d1f7f7b19305a26
chouhary12 <choudharytest2@gmail.com>
2026-07-06 19:32:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Personal-
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/God-zx/Zx-Uploader-bot--.git)
+
+
+𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗕𝗬 @Itz_Sumit
---
+++ vars.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID    = os.environ.get("API_ID", "37721193")
+API_HASH  = os.environ.get("API_HASH", "ed5cbbc0e14a777e1b2deb0c3f763874")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+CREDIT = "@Itz_Sumit"
